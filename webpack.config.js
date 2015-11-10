7b9d263bf5602caf30e49fd6da889921432e7fe2
Muhanad Al-Rubaiee <muhanadal-rubaiee@muhanads-mbp.home>
2015-11-10 23:19:45 +0000

    Instaled React and cjsx-loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
       { test: /\.scss$/, loaders: ["style", "css", "sass"] },
       { test: /\.coffee$/, loader: "coffee-loader" },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']}
     ]
   }
 }
